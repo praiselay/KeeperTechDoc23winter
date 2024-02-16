489059068dace49ca09bcdfcedcc115603e54454
redzzzi <myqueen1346@naver.com>
2024-02-17 02:45:09 +0900

    title

--- crawling.py
+++ crawling.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import pandas
 
 def crawl(url):
-  resp = requests.get('<URL>')
+  resp = requests.get(url)
   html = resp.text
   soup = BeautifulSoup(html, 'html.parser')
   # news = soup.select('.<class tag>')
