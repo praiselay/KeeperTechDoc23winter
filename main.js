a87bc7496cd03ef457a9e0770e42e35f7cfc0121
redzzzi <myqueen1346@naver.com>
2024-02-16 21:55:38 +0900

    validURL

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
 // URL 입력 후, 엔터키 입력 시 파이썬 코드 작동
 // 입력 값이 없을 경우, 안내문 필요
-var URL = document.getElementById("urlInput");
+function isValidURL(URL) {
+  var RegExp =
+    /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  if (RegExp.test(URL)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function SendURL(e) {
-    if (e.keyCode == 13) {
-        if (URL.value.length == 0) {
-            alert('Type the URL');
-            return false;
-        }
-        //파이썬 코드 실행하는 함수
-        DetectURL(URL.value);
+  if (e.keyCode == 13) {
+    var URL = document.getElementById("urlInput");
+
+    //유효한 url 검증
+    if (isValidURL(URL)) {
+      //자바스크립트의 입력값을 파이썬 코드의 인자로 전달 후 실행
+      const spawn = require("child_process").spawn;
+      const result = spawn("python", ["crawling.py"], URL);
+      console.log(result.toString());
+    } else if (URL.value.length == 0) {
+      alert("Type the URL");
+      return false;
+    } else {
+      alert("Check the URL.");
+      return false;
     }
+    //파이썬 코드 실행하는 함수
+    DetectURL(URL.value);
+  }
 }
 
 //파이썬 코드 실행하는 함수
 function DetectURL(urlValue) {
-    console.log(urlValue);
+  console.log(urlValue);
 }
