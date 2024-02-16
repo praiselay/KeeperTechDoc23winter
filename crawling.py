4b1d78d0c109a5cddab7d7f369ffd4329b1d0ce5
redzzzi <myqueen1346@naver.com>
2024-02-16 21:59:07 +0900

    crawl

--- crawling.py
+++ crawling.py
@@ -3,9 +3,12 @@ import requests
 from bs4 import BeautifulSoup
 import pandas
 
-resp = requests.get('<URL>')
-html = resp.text
-soup = BeautifulSoup(html, 'html.parser')
-# news = soup.select('.<class tag>')
+def crawl(url):
+  resp = requests.get('<URL>')
+  html = resp.text
+  soup = BeautifulSoup(html, 'html.parser')
+  # news = soup.select('.<class tag>')
+  print(soup)
 
-print(soup)
+if __name__ == '__main__':
+  crawl(sys.argv[1])
