438f44df1f5def294e84496f33ae5b1332289df0
redzzzi <myqueen1346@naver.com>
2024-02-17 23:40:56 +0900

    core setting

---
+++ test.py
@@ -0,0 +1,13 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+import pandas
+
+def crawl(url):
+  resp = requests.get(url)
+  html = resp.text
+  soup = BeautifulSoup(html, 'html.parser')
+  # news = soup.select('.<class tag>')
+  print(soup)
+
+crawl("https://keeper.or.kr/")
