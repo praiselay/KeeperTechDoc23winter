1e3c41c46ad504c62bce063ab575ce5353f9dfd7
redzzzi <myqueen1346@naver.com>
2024-02-22 01:28:58 +0900

    sonarcube

--- vulDetect.py
+++ vulDetect.py
@@ -1,3 +1,14 @@
+#Flask secret keys should not be disclosed
+
+#Credentials should not be hard-coded
+
+#Insecure temporary file creation methods should not be used
+
+#Weak SSL/TLS protocols should not be used
+
+#Applications should not create session cookies fro untrusted input
+
+
 #악성 파일 검증
 
 #중요 정보의 암호화 여부
@@ -5,5 +16,3 @@
 #SQL 삽입 위험성 판단
 
 #크로스사이트 스크립트 피해 가능성 판단
-
-#
